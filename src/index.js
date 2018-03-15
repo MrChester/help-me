0f6d53393ce4dfa1aebd48d80f96566b873e2a39
Eugene Romankov <iromankov2010@gmail.com>
2018-03-15 17:19:29 +0300

    Add N count

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 module.exports = function count(s, pairs) {
-    let bitMaskArr = s.split('');
+    let bitMaskArr = s.split(''),
+        pairsLen = pairs.length;
 
-    return result;
+    // Search N
+    let n = 1;
+    for (let i = 0; i < pairsLen; i++) {
+        for (let j = 0; j < 2; j++) {
+            n *= Math.pow(pairs[i][j], pairs[i][1])
+            break;
+        }
+    }
+
+    // const result = answer % 1000000007;
+    return /*result*/ { bitMaskArr, n };
 }
